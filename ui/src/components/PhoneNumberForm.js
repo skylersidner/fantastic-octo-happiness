50c9d82dce482abf09134aa429f1dc6a6ced6750
skylersidner <malevolentdragon@gmail.com>
2022-01-24 08:18:29 -0600

    adding input mask for phone number input

--- ui/src/components/PhoneNumberForm.js
+++ ui/src/components/PhoneNumberForm.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import PropTypes from 'prop-types';
+import InputMask from 'react-input-mask';
 
 import './PhoneNumberForm.css';
 import BasicButton from "./BasicButton";
@@ -11,7 +12,8 @@ const PhoneNumberForm = ({ onSendClick, isInvalidNumber = false, }) => {
     return (
         <form>
             <Container>
-                <input className="phone-number-input" type="tel" value={phoneNumber} onChange={e => setPhoneNumber(e.target.value)} />
+                <InputMask className="phone-number-input" mask="(999) 999-9999" value={phoneNumber} onChange={e => setPhoneNumber(e.target.value)} />
+                {/* <input className="phone-number-input" type="tel" value={phoneNumber} onChange={e => setPhoneNumber(e.target.value)} /> */}
                 {isInvalidNumber && <div className="invalid-number">Phone number is invalid.</div> }
                 <BasicButton buttonTitle="Send" clickFunction={() => onSendClick(phoneNumber)} />
             </Container>
